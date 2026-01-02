5467cc0f7cc5ecebecd9e2e226c4ee7922d447a4
Maddox <2507768velde@zuyd.nl>
2026-01-02 19:03:39 +0100

    Toevoeging classes

---
+++ Marconnes.ClassLibrary/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marconnes.ClassLibrary
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User(int userId, string name, string email)
+        {
+            UserId = userId;
+            Name = name;
+            Email = email;
+        }
+
+    }
+}
+
