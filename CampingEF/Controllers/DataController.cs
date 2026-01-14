9e7040a325655b35e64ef3362b0ce5d3ad7f57aa
Niels Theunissen <2504718theunissen@zuyd.nl>
2026-01-14 16:30:21 +0100

    Post en search toegevoegd. Weatherforecast verwijderd.

--- CampingEF/Controllers/DataController.cs
+++ CampingEF/Controllers/DataController.cs
@@ -20,5 +20,26 @@ namespace CampingEF.Controllers
         {
             return await _context.CampingPlaces.ToListAsync();
         }
+
+        [HttpGet("zoek/{zoekterm}")]
+        public async Task<ActionResult<IEnumerable<CampingPlace>>> ZoekOpNummer(string zoekterm)
+        {
+            var resultaat = await _context.CampingPlaces
+                .Where(c => c.PlaceNumber.Contains(zoekterm))
+                .ToListAsync();
+
+            return resultaat;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CampingPlace>> Toevoegen(CampingPlace nieuwePlek)
+        {
+            nieuwePlek.PlaceId = 0;
+
+            _context.CampingPlaces.Add(nieuwePlek);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAlles), new { id = nieuwePlek.PlaceId }, nieuwePlek);
+        }
     }
 }
--- CampingEF/Models/CampingPlace.cs
+++ CampingEF/Models/CampingPlace.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CampingEF.Models;
 
-public partial class CampingPlace
+public class CampingPlace
 {
     public int PlaceId { get; set; }
 
