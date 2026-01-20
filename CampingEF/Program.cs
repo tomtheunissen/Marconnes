5af20ef95058baacdbc9e560b8a12784751a1fb4
Niels Theunissen <2504718theunissen@zuyd.nl>
2026-01-20 10:22:48 +0100

    Azure API

--- CampingEF/Program.cs
+++ CampingEF/Program.cs
@@ -19,12 +19,10 @@ namespace CampingEF
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             app.UseHttpsRedirection();
 
