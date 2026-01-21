7825297961ce209d1d7c7cad93805cb6486d6246
Maddox <2507768velde@zuyd.nl>
2026-01-21 10:43:08 +0100

    API Orch.

---
+++ Orchestrator/Controllers/OrchestratorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Orchestrator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrchestratorController : ControllerBase
+    {
+        
+    }
+}
