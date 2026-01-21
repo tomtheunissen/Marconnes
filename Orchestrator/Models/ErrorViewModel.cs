4dc509495cb784fb1f8c9ce9a636f67d668d1504
Maddox <2507768velde@zuyd.nl>
2026-01-21 09:55:36 +0100

    orchest.

---
+++ Orchestrator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Orchestrator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
